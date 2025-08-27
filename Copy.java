f8a9e500bb55ac109b5593362b495f21534953a7
cactus-beep-beep <cameronbak09@gmail.com>
2025-08-27 13:47:15 -0700

    Create Copy.java

---
+++ Copy.java
@@ -0,0 +1,22 @@
+/*
+AP CS A
+Name: [FIRST AND LAST NAME]
+Date: [CURRENT DATE]
+Teacher: Ms. Gee
+Assignment: Project 0 - Hello World!
+Description: [WRITE ANYTHING APPROPIATE HERE.]
+*/
+
+public class Copy {
+  public static void main(string[]args) {
+
+    int x = 0;
+    int y ;
+    
+    y=x;  // how do I copy the information from x into the variable y?
+
+    System.out.println("x: " + x); //print before you copy
+    System.out.println(y: " + y);
+
+  }
+}
